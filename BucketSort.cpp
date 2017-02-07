edcb4a713000c5acb85142cadd6e710ab82a9190
Adalberto Belmiro de Souto Neto <adalberto.belmiro@cc.ci.ufpb.br>
2017-02-07 11:42:52 -0300

    Add files via upload

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -14,6 +14,19 @@ void bucketsort(int vetor[], int n){
     for(int i=0; i<n; ++i)
         ++buckets[vetor[i]];
 
+    for(int i=0,j =0; j<range; ++j)
+        for(int k = buckets[j]; k>0; --k)
+            vetor[i++] = j;
+}
 
+int main(){
+    int input_vetor[] = {10, 24, 22, 62, 1, 50, 100, 75, 2, 3};
+    int n = sizeof (input_vetor)/ sizeof(input_vetor[0]);
+    bucketsort(input_vetor, n);
 
+    cout<<"Sorted Array: "<<endl;
+    for(int i=0; i<n; ++i){
+        cout<< input_vetor[i] <<" "<<endl;
+    }
+    return 0;
 }
