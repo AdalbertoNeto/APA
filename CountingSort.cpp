26bddce49d7b6bb53ba673641d98d42a608985be
Adalberto Belmiro de Souto Neto <adalberto.belmiro@cc.ci.ufpb.br>
2017-02-07 10:14:45 -0300

    Add files via upload

---
+++ CountingSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int INPUT_SIZE = 20;
+const int BUCKET_K = 10;
+
+void MostraVetor(int *input){
+    for(int i =0; i< INPUT_SIZE; i++)
+        cout << input[i] << " " << endl;
+}
+
+void countingsort(int* input){
+    int CountVetor[BUCKET_K] = { 0 };
+
+
+
+}
