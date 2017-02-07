067fb98a9922d80ef8e13d26f1768c72a77f7cdf
Adalberto Belmiro de Souto Neto <adalberto.belmiro@cc.ci.ufpb.br>
2017-02-07 11:54:28 -0300

    Add files via upload

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -1,10 +1,12 @@
 //www.sanfoundry.com
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 void bucketsort(int vetor[], int n){
 
-    int range = 101;
+    int range = 10000;
     int buckets[range];
 
     for(int i=0; i<range; ++i){
@@ -20,13 +22,25 @@ void bucketsort(int vetor[], int n){
 }
 
 int main(){
-    int input_vetor[] = {10, 24, 22, 62, 1, 50, 100, 75, 2, 3};
-    int n = sizeof (input_vetor)/ sizeof(input_vetor[0]);
-    bucketsort(input_vetor, n);
+    ifstream file("example.in");
+    if(!file){/* Falhou em abrir o arquivo */}
+
+    int tamanho;
+    if(!(file >> tamanho)){/*Falhou em ler o primeiro valor */}
+
+    int* vetor = new int[tamanho];
+    for(int i=0; i<tamanho; ++i){
+        if(!(file>>vetor[i])){/*Falhou em ler um dos valores */}
+    }
+
+
+    //int input_vetor[] = {10, 24, 22, 62, 1, 50, 100, 75, 2, 3};
+    //int n = sizeof (input_vetor)/ sizeof(input_vetor[0]);
+    bucketsort(vetor, tamanho);
 
     cout<<"Sorted Array: "<<endl;
-    for(int i=0; i<n; ++i){
-        cout<< input_vetor[i] <<" "<<endl;
+    for(int i=0; i<tamanho; ++i){
+        cout<< vetor[i] <<" "<<endl;
     }
     return 0;
 }
