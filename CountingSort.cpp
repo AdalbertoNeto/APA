4e2e5bb88ea06495ff61c10cbf694ce3e4a6f33d
Adalberto Belmiro de Souto Neto <adalberto.belmiro@cc.ci.ufpb.br>
2017-02-07 11:12:55 -0300

    Update CountingSort.cpp

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //www.sourcetricks.com
 #include <iostream>
 #include <fstream>
@@ -49,43 +48,3 @@ int main(){
 
     return 0;
 }
-=======
-#include <iostream>
-
-using namespace std;
-
-const int INPUT_SIZE = 20;
-const int BUCKET_K = 10;
-
-void MostraVetor(int *input){
-    for(int i =0; i< INPUT_SIZE; i++)
-        cout << input[i] << " " << endl;
-}
-
-void countingsort(int* input){
-    int CountVetor[BUCKET_K] = { 0 };
-
-    for(int i=0; i<INPUT_SIZE; i++){
-        CountVetor[input[i]]++;
-    }
-
-    int outputIndex = 0;
-    for(int j=0; j<BUCKET_K; j++){
-        while(CountVetor[j]--)
-            input[outputIndex++] = j;
-    }
-}
-
-int main(){
-    int input[INPUT_SIZE] = {2,4,6,4,7,1,4,9,5,3,7,2,2,6,9,3,7,3,4,4};
-
-    cout<<"Input: ";
-    MostraVetor(input);
-
-    countingsort(input);
-    cout<<"Output: ";
-    MostraVetor(input);
-
-    return 0;
-}
->>>>>>> origin/master
